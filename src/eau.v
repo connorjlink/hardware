f07cca9093e563129f8621401af086e7534376de
Connor Link <connorlink248@gmail.com>
2022-08-27 13:04:33 -0500

    Update eau.v

--- src/eau.v
+++ src/eau.v
@@ -1,6 +1,29 @@
 `include "../util/register.v"
 
+
+
+
+
 module eauD_A(input[7:0] d, input di, clk, hs, ls, ao, output[15:0] a);
+    wire[15:0] q;
+    
+    register_rst rl
+    (
+        d, 
+        clk, 
+        ls, 
+        q[7:0]
+    );
+
+    register_rst rh
+    (
+        d,
+        clk,
+        hs,
+        q[15:8]
+    );
+
+    assign a = q;
 
 endmodule
 
